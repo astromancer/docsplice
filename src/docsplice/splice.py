e06e5b3984dcf8567ec3f3ecc69b727ff999fbfe
astromancer <hannes@saao.ac.za>
2021-03-27 11:09:59 +0200

    refactor and cleanup

--- src/docsplice/splice.py
+++ src/docsplice/splice.py
@@ -1,10 +1,17 @@
 """
-Docstring splicing.  Currently only numpydoc style supported.
+Docstring splicing and fstring-like substitutions.
 """
 
+from collections import defaultdict
 import re
 import warnings
 from numpydoc.docscrape import NumpyDocString
+import logging
+
+# module level logger
+logging.basicConfig()
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
 
 
 TAB = ' ' * 4
@@ -33,46 +40,176 @@ FORMATTERS = {
         'index':                NumpyDocString._str_index}
 }
 
-# FORMATTERS = {
-#     'Signature':            NumpyDocString._str_signature,
-#     'Summary':              NumpyDocString._str_summary,
-#     'Extended Summary':     NumpyDocString._str_extended_summary,
-#     'Parameters':           NumpyDocString._str_param_list,
-#     'Returns':              NumpyDocString._str_param_list,
-#     'Yields':               NumpyDocString._str_param_list,
-#     'Receives':             NumpyDocString._str_param_list,
-#     'Other Parameters':     NumpyDocString._str_param_list,
-#     'Raises':               NumpyDocString._str_param_list,
-#     'Warns':                NumpyDocString._str_param_list,
-#     'Warnings':             NumpyDocString._str_section,
-#     'See Also':             NumpyDocString._str_see_also,
-#     'Notes':                NumpyDocString._str_section,
-#     'References':           NumpyDocString._str_section,
-#     'Examples':             NumpyDocString._str_section,
-#     'Attributes':           NumpyDocString._str_param_list,
-#     'Methods':              NumpyDocString._str_param_list,
-#     'index':                NumpyDocString._str_index
-# }
 
-PARSER = re.compile(
-    r"""(?xmi)
-    (^\s*)
-    \{
-        ([A-Z][a-z ]+)
-        (?:\[(\w+)\])?
-        (?:\.(\w+))?
-        (?:\s+as\s+(\w+))?
-    \}
-    """)
+# ---------------------------------------------------------------------------- #
+# helpers
+
+def indented(lines, indent=TAB):
+    return f'\n{indent}'.join(lines).rstrip()
+
+
+def format_param(name, kind, descr, indent=TAB):
+    return indented([' : '.join((name, kind))] + descr, indent)
+
+# def parse_examples # TODO
+
+
+# ---------------------------------------------------------------------------- #
+# cache
+
+class DocStringCache(defaultdict):
+    """Cache for parsed docs"""
+
+    def __init__(self, *args, **kws):
+        super().__init__(NumpyDocString, *args, **kws)
+
+    def __missing__(self, key):
+        new = self[key] = self.default_factory(key)
+        return new
+
+
+# Module scoped cache
+docStringCache = DocStringCache()
+
+# ---------------------------------------------------------------------------- #
+# directives
+
+
+class Directive:
+    """
+    A substitution directive in a style similar to fstrings.  These are bits of
+    string in the docustring that specify which parts of the incoming docstring
+    to substitute.
+
+    For example:
+        {Summary}
+        {Parameters[name] as new_name}
+        {Returns[result].desr}
+
+    These will use be replaced by the corresponding values in the parsed 
+    `NumpyDocString` of the `from_func`
+    """
+
+    regex = re.compile(
+        r"""(?xm)
+        (?P<indent>^\s*)
+        (?P<directive>
+            \{
+                (?i:(?P<section>[a-z]+))
+                (\[(?P<key>\w+)\])?
+                (\.(?P<attr>\w+))?
+                (\s+as\s+(?P<rename>\w+))?
+            \}
+        )
+        """)
+
+    # Attributes
+    directive: str
+    indent: str
+    section: str
+    key: str
+    attr: str
+    rename: str
+
+    @classmethod
+    def parse(cls, string):
+        if (string[0] + string[-1]) != '{}':
+            string = string.join('{}')
+
+        match = cls.regex.match(string)
+        if match is None:
+            raise ValueError(f'Directive {string!r} could not be parsed!')
+
+        return cls(**match.groupdict())
+
+    @classmethod
+    def iter(cls, docstring):
+        for match in cls.regex.finditer(docstring):
+            yield cls(**match.groupdict())
+
+    def __init__(self, **parts):
+        self.__dict__.update(**parts)
+        section, key, attr = self.section, self.key, self.attr
+        self.section = section.title()
+        if not self.section in NumpyDocString.sections:
+
+            raise ValueError(f'{self.section} is not a valid section name.')
+
+        if section not in LISTED_SECTIONS:
+            if key:
+                warnings.warn(
+                    f'{section!r} section is not a itemized section, yet item '
+                    f'{key!r} has been requested.')
+                if attr:
+                    warnings.warn(
+                        f'Attribute {attr!r} of item {key!r} in section '
+                        f'{section!r} does not exist')
+
+    def __str__(self):
+        return self.directive
+
+    def __iter__(self):
+        yield from self.parts
+
+    @property
+    def parts(self):
+        return (self.indent, self.section, self.key, self.attr, self.rename)
+
+    def get_replacement(self, parsed_doc):
+        directive = self.directive
+        indent, section, key, attr, rename = self
+
+        if section not in parsed_doc:
+            warnings.warn(f'Invalid docstring section {section!r}')
+            return directive
+
+        part = parsed_doc[section]
+        formatter = FORMATTERS[section]
+        takes_param = (section in LISTED_SECTIONS + STRING_SECTIONS)
+        args = (parsed_doc, *(section, )[:takes_param])
+
+        if not key:
+            return indented(formatter(*args), indent)
+
+        has_items = (section in LISTED_SECTIONS)
+        if not has_items:
+            warnings.warn(f'{section!r} section has no items. Could not lookup '
+                          f'item {key!r}.')
+            return directive
+
+        # check if item (parameter) available
+        names = next(zip(*part))
+        if not key in names:
+            warnings.warn(f'Could not find {key!r} in section {section!r}')
+            return directive
+
+        # get item
+        i = names.index(key)
+        item = part[i]  # parameter
+        if not attr:
+            return format_param(rename or item.name,
+                                item.type,
+                                item.desc,
+                                TAB + indent)
+
+        if hasattr(item, attr):
+            # get the attribute (decription or whatever)
+            return indented(getattr(item, attr), indent)
+
+        warnings.warn(f'{section}[{key}] has no attribute {attr!r}.')
+        return directive
+
+# ---------------------------------------------------------------------------- #
+# decorator
 
 
 class splice:
     """
-    Docstring splicing for callables following numpydoc convention.
+    Decorator for splicing numpydoc style docstrings.
     """
 
     def __init__(self, from_func, *sections, insert=None, replace=None,
-                 omit=(), preserve_order=False):
+                 omit=(), onfail=logger.warning, preserve_order=False):
         """
         Initialize splice function decorator.
 
@@ -85,6 +222,8 @@ class splice:
             sections from various other sources, by default None.
         replace : dict, optional
             Verbatim substitution mapping str -> str, by default None.
+        onfail : callable, optional
+            What to do when the decorator throws an exception
         preserve_order : bool, optional
             Whether the order of the sections should be preserved as they appear 
             in the original docstring. If False, the sections will be ordered as 
@@ -100,7 +239,7 @@ class splice:
                 Raises        
                 Warns         
                 Warnings      
-                See Al        
+                See Also        
                 Notes         
                 References    
                 Examples      
@@ -112,156 +251,110 @@ class splice:
         """
         self.from_func = from_func
         self.origin = NumpyDocString(from_func.__doc__)
-        self.replace = replace or {}
-        self.omit = (omit, ) if isinstance(omit, str) else tuple(omit)
-        self.insert = insert or {}
+        self.replacement = replace or {}
+        self.removal = (omit, ) if isinstance(omit, str) else tuple(omit)
+        self.insertion = insert or {}
         if len(sections) == 1 and isinstance(sections[0], dict):
-            self.insert.update(sections[0])
+            self.insertion.update(sections[0])
         else:
-            self.insert.update({s.title(): from_func for s in sections})
+            self.insertion.update(
+                {s.capitalize(): from_func for s in sections})
+
+        self.exception_hook = onfail
 
     def __call__(self, func):
+        try:
+            return self.splice(func)
+        except Exception as err:
+            self.exception_hook(err)
+            return func
+
+    def splice(self, func):
         """
         Main work of creating or updating the function docstring happens here.
         This is a signature preserving decorator that only alters the `__doc__`
         attribute of the object.
         """
+
+        # docstring of decorated function. The one to be adapted.
+        docstring = func.__doc__
+
+        # make substitutions
+        docstring = self.replace(docstring)
+
+        # parse the update docstring
+        doc = NumpyDocString(docstring)
+
+        # parse directives and insert
+        doc = self.insert(func, doc)
+
+        # remove omitted sections / parameters
+        self.remove(doc)
+
+        # write the new docstring
+        if (self.replacement or self.insertion):
+            func.__doc__ = str(doc)
+        else:
+            warnings.warn(
+                f'{self.__class__.__name__} did not alter docstring for {func}.'
+            )
+
+        return func
+
+    def replace(self, docstring):
         # avoid circular import :|
         from recipes.string import sub
 
-        replacements = self.replace
-
-        # docstring of decorated function. The one to be adapted
-        docstring = func.__doc__
+        # First find directives in the docstring of the decorated function and,
+        # # substitute the replacement texts
         if docstring is None:
-            # carbon copy if decorated function has no docstring
+            # if decorated function has no docstring carbon copy the docstring
+            # from input source
             docstring = self.from_func.__doc__
         else:
-            # find directives in the docstring of the decorated function and
-            # get their replacement texts
-            for match in PARSER.finditer(docstring):
-                directive = match[0].lstrip()
-                replacements[directive] = get_replacement(self.origin, match)
+            for directive in Directive.iter(docstring):
+                self.replacement[str(directive)] = directive.get_replacement(
+                    self.origin)
 
-        # make substitutions
-        docstring = sub(docstring, replacements)
+        docstring = sub(docstring, self.replacement)
+        return docstring
 
+    def insert(self, func, doc):
         # parse the altered docstring
-        doc = NumpyDocString(docstring)  # decoratee
+
         idocs = {}  # docs from which parts will be scraped
-        # TODO: make this cache session scoped
-        # parse insertion directives
-        for directive, ifunc in self.insert.items():
+
+        for directive, ifunc in self.insertion.items():
             if isinstance(ifunc, type):
                 ifunc = ifunc.__init__
-
             if ifunc in idocs:
                 idoc = idocs[ifunc]
             else:
                 idoc = NumpyDocString(ifunc.__doc__)
 
-            parse(directive)
-            new = get_replacement(idoc, match)
-
+            directive = _, section, key, _, rename = Directive.parse(directive)
+            new = directive.get_replacement(idoc)
             if section in LISTED_SECTIONS:
                 # read the new parameter
                 new = doc._parse_param_list(new)
                 if section == 'Parameters':
                     # find position of new parameter and insert it in the list
-                    param_names = func.__code__.co_varnames
-                    i = param_names.index(rename or key) - \
-                        isinstance(func, type)
+                    par_names = func.__code__.co_varnames
+                    i = par_names.index(rename or key) - isinstance(func, type)
                     part = doc[section]
                     doc[section] = part[:i] + new + part[i:]
                 else:
-                    doc[section].append(new)
+                    doc[section].extend(new)
             else:
                 doc[section] = new
+        return doc
 
-        # remove requested sections
-        if self.omit:
-            match = PARSER.match(directive)
-            indent, section, key, attr, rename = match[2], match[3], match[4]
+    def remove(self, doc):
+        for directive in self.removal:
+            _, section, key, attr, _ = Directive.parse(directive)
             part = doc[section]
             if key:
-                part.pop(key)
-
-        if (replacements or self.insert):
-            func.__doc__ = str(doc)
-        else:
-            warnings.warn(
-                f'{self.__class__.__name__} did not make any substitutions in '
-                f' docstring for {func}.'
-            )
-
-        return func
-
-
-def parse(directive):
-    if (directive[0] + directive[-1]) != '{}':
-        directive = directive.join('{}')
-
-    match = PARSER.match(directive)
-    indent, section, key, attr, rename = match.groups()
-    if section not in LISTED_SECTIONS:
-        if key: 
-            warnings.warn(
-                f'{section!r} section is not a itemized section, yet item {key!r} has been requested.')
-            if attr:
-                warnings.warn(
-                f'Attribute {attr!r} of item {key!r} in section {section!r} does not exist')
-            
-            
-    return indent, section, key, attr, rename
-
-
-def get_replacement(parsed, match):
-    indent, section, key, attr, rename = match.groups()
-    directive = match[0].lstrip()
-    if section not in parsed:
-        warnings.warn(f'Invalid docstring section {section!r}')
-        return directive
-
-    part = parsed[section]
-    formatter = FORMATTERS[section]
-    takes_param = (section in LISTED_SECTIONS + STRING_SECTIONS)
-    args = (parsed, *(section, )[:takes_param])
-
-    if not key:
-        return indented(formatter(*args), indent)
-
-    has_items = (section in LISTED_SECTIONS)
-    if not has_items:
-        warnings.warn(f'{section!r} section has no items. Could not lookup '
-                      f'item {key!r}.')
-        return directive
-
-    # check if item (parameter) available
-    names = next(zip(*part))
-    if not key in names:
-        warnings.warn(f'Could not find {key!r} in section {section!r}')
-        return directive
-
-    # get item
-    i = names.index(key)
-    item = part[i]  # parameter
-    if not attr:
-        return format_param(rename or item.name,
-                            item.type,
-                            item.desc,
-                            TAB + indent)
-
-    if hasattr(item, attr):
-        # get the attribute (decription or whatever)
-        return indented(getattr(item, attr), indent)
-
-    warnings.warn(f'{section}[{key}] has no attribute {attr!r}.')
-    return directive
-
-
-def indented(lines, indent=TAB):
-    return f'\n{indent}'.join(lines).rstrip()
-
-
-def format_param(name, kind, descr, indent=TAB):
-    return indented([' : '.join((name, kind))] + descr, indent)
+                if attr:
+                    part[key]. attr = ''
+                else:
+                    part[key] = doc.sections[key]
